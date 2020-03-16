f6d08c6ee6647e908b9bbd7ed70fc192cfdeefaa
Ingvordsen <ing06d@csiro.au>
2020-03-17 10:43:52 +1100

    starting code script

---
+++ code-week2.R
@@ -0,0 +1,14 @@
+
+
+
+
+library(tidyverse)
+
+
+#For each station, how many days have a minimum temperature, a maximum temperature and a rainfall measurement recorded?
+
+
+bom_date <- read_csv("C:/Users/ing06d/Documents/Data school FOCUS/Data-week2/Data-school-week2/BOM_data.csv")
+
+
+bom_stations <- read_csv("C:/Users/ing06d/Documents/Data school FOCUS/Data-week2/Data-school-week2/BOM_stations.csv")
