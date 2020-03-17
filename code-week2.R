82f7fb486be7fa58f097a19c644142ccfc12b612
Ingvordsen <ing06d@csiro.au>
2020-03-17 14:47:53 +1100

    Question 2 answered

--- code-week2.R
+++ code-week2.R
@@ -1,7 +1,4 @@
 
-
-
-
 library(tidyverse)
 
 
@@ -23,24 +20,33 @@ head(bom_date)
 
 bom_data_sep <- 
             bom_data %>% 
-              separate(Temp_min_max, into = c('tmp_min', 'tmp_max'), sep = '/')    #splitting the combined max&min tmp
+              separate(Temp_min_max, into = c('tmp_min', 'tmp_max'), sep = '/') %>%    #splitting the combined max&min tmp
               filter(tmp_min != '-', tmp_max != '-', Rainfall != '-', Solar_exposure !='-')  #excluding all rows with missing values
 
-
-answerQ1 <- 
+  #filter (is.na(tmp_min, tmp_max)) can only be used when a dataframe/tibble has NA in it - one can replace eg - with NA
+              
             bom_data_sep %>% 
               group_by(Station_number) %>%  #NOOOOO '' when grouping
-              summarise('Day' = n())
+              summarise('Day' = n()) %>% 
+              write_csv("C:/Users/ing06d/Documents/Data school FOCUS/Data-week2/Outputs/answerQ1.csv")
 
-  
-
-view(answerQ1)
 
 ##Question2 ##
 #Which month saw the lowest average daily temperature difference?
 
+bom_data_difftmp <- 
+   bom_data_sep %>%   
+        mutate_at(vars(tmp_min, tmp_max, Rainfall, Solar_exposure), as.numeric) %>%  # change values/measurements to numbers
+        mutate(diff_daily_tmp = tmp_max - tmp_min) %>%   # create tmp_diff column
+        group_by(Month) %>% 
+        summarize(mean_month_tmp = mean(diff_daily_tmp)) %>% 
+        write_csv("C:/Users/ing06d/Documents/Data school FOCUS/Data-week2/Outputs/answerQ2.csv")
 
 
-  
+## Question 3 ##
+# Which state saw the lowest average daily temperature difference?
+
+
+
   
   
