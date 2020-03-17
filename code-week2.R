e3252652c4d221534e9716d95898ccab20c77eed
Ingvordsen <ing06d@csiro.au>
2020-03-17 11:55:52 +1100

    used separate and filter and summarise to answer question one

--- code-week2.R
+++ code-week2.R
@@ -5,10 +5,28 @@
 library(tidyverse)
 
 
-#For each station, how many days have a minimum temperature, a maximum temperature and a rainfall measurement recorded?
+######### CHALLENGE ########
+##For each station, how many days have a minimum temperature, a maximum temperature and a rainfall measurement recorded?
 
 
-bom_date <- read_csv("C:/Users/ing06d/Documents/Data school FOCUS/Data-week2/Data-school-week2/BOM_data.csv")
+bom_data <- read_csv("C:/Users/ing06d/Documents/Data school FOCUS/Data-week2/Data-school-week2/BOM_data.csv")
 
 
 bom_stations <- read_csv("C:/Users/ing06d/Documents/Data school FOCUS/Data-week2/Data-school-week2/BOM_stations.csv")
+
+
+#need to separate temperature column
+
+
+head(bom_date)
+
+bom_data_sep <- 
+            bom_data %>% 
+              separate(Temp_min_max, into = c('tmp_min', 'tmp_max'), sep = '/') %>%     #splitting the combined max&min tmp
+              filter(tmp_min != '-', tmp_max != '-', Rainfall != '-', Solar_exposure !='-')  %>%   #excluding all rows with missing values                
+              summarise('Station_number' = tmp_min)
+
+view(bom_data_sep)
+  
+  
+  
